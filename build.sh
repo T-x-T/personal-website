b19d18dd65ae40bc0743b957941910c00d73b181
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-11-28 20:58:22 +0100

    add header and footer, subdirectory support to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+rm -r .out
 mkdir .out
-cp src/* .out/
+
 cp static/* .out/
+
+for filename in src/* src/**/*; do
+	if [[ "$filename" != "src/#header.html" && "$filename" != "src/#footer.html" && -f "$filename" ]]; then
+		echo "$filename"
+		cat "src/#header.html" "$filename" "src/#footer.html" > temp
+		install -D temp "${filename/#src/.out}"
+		rm temp
+	fi
+done
