8619a1ad08d8339d32fdf002fb286e1e70fb8113
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-11-29 20:38:40 +0100

    rough outline

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ cp static/* .out/
 
 for filename in src/* src/**/*; do
 	if [[ "$filename" != "src/#header.html" && "$filename" != "src/#footer.html" && -f "$filename" ]]; then
-		echo "$filename"
 		cat "src/#header.html" "$filename" "src/#footer.html" > temp
 		install -D temp "${filename/#src/.out}"
 		rm temp
