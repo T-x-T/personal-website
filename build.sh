f6bc3b10cc9123213e9abc4e18c29263f58e50ad
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-11-30 21:52:19 +0100

    Added dynamic titles to build script

--- build.sh
+++ build.sh
@@ -4,9 +4,25 @@ mkdir .out
 
 cp static/* .out/
 
+header=$(cat src/#header.html)
+footer=$(cat src/#footer.html)
+
 for filename in src/* src/**/*; do
 	if [[ "$filename" != "src/#header.html" && "$filename" != "src/#footer.html" && -f "$filename" ]]; then
-		cat "src/#header.html" "$filename" "src/#footer.html" > temp
+		content=$(cat "$filename")
+
+		comments=$(grep -oP "(?<=<\!\--\s)(.*?)(?=\s-->)" <<< "$content")
+		properties=$(grep -oP "(?<=\\$&)(.*?)(?=&\\$)" <<< "$comments")
+
+		while read -r comment; do
+			if [[ $comment =~ title:(.*) ]]; then
+				title="${BASH_REMATCH[1]} - TxTs website"
+				titled_header="${header//$&title&$/$title}"
+				echo "$titled_header"
+			fi
+		done <<< "$properties"
+
+		echo "$titled_header$content$footer" > temp
 		install -D temp "${filename/#src/.out}"
 		rm temp
 	fi
